e2914d8e6bac0bd23446e38cecf846bc50f574d7
Maulik Ramani <maulik.ramani@infocusp.com>
2025-10-03 13:41:59 +0530

    Change path

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
+    plugins: [].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    base: "/portfolio/",
 }));
